e9a14a844a8eab7418c00e324f556feb28fcfd15
Ivan <frontenddeveloperweb@gmail.com>
2020-10-20 15:33:33 +0500

    Добавлено удаление элементов

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -2,32 +2,49 @@ import React, {Component} from 'react';
 import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
-  constructor() {
-    super();
-    this.onLabelClick = () => {
-      console.log(`Clicked on: ${this.props.label}`);
-    }
-  }
+  state = {
+    done: false,
+    important: false
+  };
+  onLabelClick = () => {
+    this.setState(({ done }) => {
+      return {
+        done: !done
+      };
+    });
+  };
+  onMarkImportant = () => {
+    this.setState(({ important }) => {
+      return {
+        important: !important
+      };
+    });
+  };
   render() {
-    const { label, important = false } = this.props;
-    const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
-    };
+    const { label, onDeleted } = this.props;
+    const { done, important } = this.state;
+    let classNames = 'todo-list-item';
+    if (done) {
+      classNames += ' done';
+    }
+    if (important) {
+      classNames += ' important';
+    }
+
     return (
-      <span className="todo-list-item">
+      <span className={classNames}>
         <span
           className="todo-list-item-label"
-          style={style}
           onClick={ this.onLabelClick }>
           {label}
         </span>
-        <button type="button"
+        <button onClick={ this.onMarkImportant } type="button"
                 className="btn btn-outline-success btn-sm float-right">
           <i className="fa fa-exclamation" />
         </button>
         <button type="button"
-                className="btn btn-outline-danger btn-sm float-right">
+                className="btn btn-outline-danger btn-sm float-right"
+                onClick={onDeleted}>
           <i className="fa fa-trash-o" />
         </button>
       </span>
